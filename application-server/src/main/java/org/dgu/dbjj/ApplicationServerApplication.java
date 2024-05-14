edd00a8bca70640a9bd8775af3469fd3f5528110
nykoh2001 <nykoh2001@gmail.com>
2024-05-14 21:44:38 +0900

    [CHORE] #0 project setting

---
+++ application-server/src/main/java/org/dgu/dbjj/ApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package org.dgu.dbjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationServerApplication.class, args);
+    }
+
+}
---
+++ application-server/src/test/java/org/dgu/dbjj/ApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dgu.dbjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
