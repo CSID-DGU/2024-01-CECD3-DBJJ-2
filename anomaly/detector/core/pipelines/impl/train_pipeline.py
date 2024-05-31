abe5d3a0b7df5e2c94c7c029bc864b854aab5823
nykoh2001 <nykoh2001@gmail.com>
2024-05-31 16:06:16 +0900

    [FEAT] #3 pipeline

--- anomaly/detector/core/pipelines/impl/train_pipeline.py
+++ anomaly/detector/core/pipelines/impl/train_pipeline.py
@@ -2,5 +2,13 @@ from core.pipelines.pipeline import Pipeline
 
 
 class TrainPipeline(Pipeline):
+    _instance = None
+
+    @classmethod
+    def get_instance(cls) -> Pipeline:
+        if cls._instance is None:
+            cls._instance = TrainPipeline()
+        return cls._instance
+
     def run(self):
         return None
--- anomaly/detector/core/pipelines/pipeline.py
+++ anomaly/detector/core/pipelines/pipeline.py
@@ -2,6 +2,9 @@ from abc import abstractmethod, ABCMeta
 
 
 class Pipeline(metaclass=ABCMeta):
+    @abstractmethod
+    def get_instance(self):
+        pass
 
     @abstractmethod
     def run(self):
