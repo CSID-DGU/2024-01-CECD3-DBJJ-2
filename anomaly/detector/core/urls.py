258e6158ed617944dd0b8ce0fa4b6479a3c955cd
nykoh2001 <nykoh2001@gmail.com>
2024-05-31 16:05:46 +0900

    [FEAT] #3 train/predict api

--- anomaly/detector/core/urls.py
+++ anomaly/detector/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path('train', views.train, name="train"),
+    path('predict', views.predict, name="predict")
 ]
--- anomaly/detector/core/views.py
+++ anomaly/detector/core/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
 
-from core.pipelines.impl.predict_pipeline import PredictPipeline
-from core.pipelines.impl.train_pipeline import TrainPipeline
 from core.pipelines.pipeline import Pipeline
+from core.pipelines.pipeline_proxy import PipelineProxy
 
 
 # Create your views here.
-def train(request: HttpRequest) -> HttpResponse:
-    pipeline: Pipeline = TrainPipeline()
+def train() -> HttpResponse:
+    # PipelineProxy.train()
+    return JsonResponse(
+        {
+            "status": "success"
+        }
+    )
 
 def predict(request: HttpRequest) -> HttpResponse:
-    pipeline: Pipeline = PredictPipeline()
+    # PipelineProxy.predict()
+    return JsonResponse(
+        {
+            "status": "success"
+        }
+    )
