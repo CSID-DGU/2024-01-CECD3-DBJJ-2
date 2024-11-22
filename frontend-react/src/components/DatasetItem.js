8e92f6936c82fcd5791e61139e48af8f4a6ad96e
euneun17 <0617ye@gmail.com>
2024-11-22 14:52:36 +0900

    Dataset 페이지 수정

--- frontend-react/src/components/DatasetItem.js
+++ frontend-react/src/components/DatasetItem.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import '../styles/DatasetItem.css';
 
-const DatasetItem = ({ item }) => {
+const DatasetItem = ({ item, onTypeChange }) => {
   return (
     <>
       <tr className="dataset-group">
         <td colSpan="2">{item.name}</td>
-        <td>{item.type}</td>
+        <td>
+          {item.name === 'Bottle' ? (
+            'Structural' // Bottle의 type은 고정
+          ) : (
+            // Pushpins의 type은 라디오 버튼으로 선택 가능
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name={`type-${item.name}`}
+                  value="Pair"
+                  checked={item.type === 'Pair'}
+                  onChange={() => onTypeChange('Pair')}
+                />
+                Pair
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name={`type-${item.name}`}
+                  value="Num"
+                  checked={item.type === 'Num'}
+                  onChange={() => onTypeChange('Num')}
+                />
+                Num
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name={`type-${item.name}`}
+                  value="Portion"
+                  checked={item.type === 'Portion'}
+                  onChange={() => onTypeChange('Portion')}
+                />
+                Portion
+              </label>
+            </div>
+          )}
+        </td>
         <td>{item.size}</td>
         <td>{item.date}</td>
         <td>
--- frontend-react/src/pages/DataSet.js
+++ frontend-react/src/pages/DataSet.js
@@ -1,54 +1,97 @@
-// src/pages/Dataset.js
-import React from 'react';
+import React, { useState } from 'react';
 import DatasetItem from '../components/DatasetItem';
 import '../styles/Dataset.css';
 
 const Dataset = () => {
-  const data = [
+  const [data, setData] = useState([
     {
       name: 'Bottle',
-      type: 'Structural',
+      type: 'Structural', // Structural로 고정
       size: '180MB',
       date: '03/10/2024',
       details: [
-        { tags: 'Train', detailType: 'good', size: '10.0MB', date: '03/10/2024' },
-        { tags: 'Test', detailType: 'broken_large', size: '15.6MB', date: '03/10/2024' },
-        { tags: 'Test', detailType: 'broken_small', size: '28.0MB', date: '03/10/2024' },
-        { tags: 'Test', detailType: 'contamination', size: '19.0MB', date: '03/10/2024' },
+        { tags: 'Train', detailType: ' ', size: '10.0MB', date: '03/10/2024' },
+        { tags: 'Test', detailType: ' ', size: '15.6MB', date: '03/10/2024' },
+        { tags: 'Test', detailType: ' ', size: '28.0MB', date: '03/10/2024' },
+        { tags: 'Test', detailType: ' ', size: '19.0MB', date: '03/10/2024' },
       ],
     },
     {
-      name: 'pushpins',
-      type: 'Logical',
+      name: 'Pushpins',
+      type: 'Logical', // 라디오 버튼으로 선택 가능
       size: '180MB',
       date: '03/10/2024',
       details: [
-        { tags: 'Train', detailType: 'good', size: '10.0MB', date: '03/10/2024' },
-        { tags: 'Test', detailType: 'good', size: '10.0MB', date: '03/10/2024' },
-        { tags: 'Test', detailType: 'logical_anomalies', size: '10.0MB', date: '03/10/2024' },
+        { tags: 'Train', detailType: ' ', size: '10.0MB', date: '03/10/2024' },
+        { tags: 'Test', detailType: ' ', size: '10.0MB', date: '03/10/2024' },
+        { tags: 'Test', detailType: ' ', size: '10.0MB', date: '03/10/2024' },
       ],
     },
-  ];
+  ]);
+
+  const handleTypeChange = (itemIndex, newType) => {
+    const updatedData = [...data];
+    updatedData[itemIndex].type = newType;
+    setData(updatedData);
+  };
+
+  // Structural과 Logical 데이터를 필터링
+  const structuralData = data.filter((item) => item.name === 'Bottle');
+  const logicalData = data.filter((item) => item.name === 'Pushpins');
 
   return (
-    <div className="dataset">
-      <table className="dataset-table">
-        <thead>
-          <tr>
-            <th>Data Name</th>
-            <th>Tags</th>
-            <th>Detail Type</th>
-            <th>Size</th>
-            <th>Date</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <DatasetItem key={index} item={item} />
-          ))}
-        </tbody>
-      </table>
+    <div className="full">
+      {/* Structural Section */}
+      <div className="dataset">
+        <h2>Structural</h2>
+        <table className="dataset-table">
+          <thead>
+            <tr>
+              <th>Data Name</th>
+              <th>Tags</th>
+              <th>Detection Type</th>
+              <th>Size</th>
+              <th>Date</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {structuralData.map((item, index) => (
+              <DatasetItem
+                key={index}
+                item={item}
+                onTypeChange={() => {}} // Structural은 고정이므로 변경 불가
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Logical Section */}
+      <div className="dataset">
+        <h2>Logical</h2>
+        <table className="dataset-table">
+          <thead>
+            <tr>
+              <th>Data Name</th>
+              <th>Tags</th>
+              <th>Detection Type</th>
+              <th>Size</th>
+              <th>Date</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {logicalData.map((item, index) => (
+              <DatasetItem
+                key={index}
+                item={item}
+                onTypeChange={(newType) => handleTypeChange(index, newType)}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
